3aec04e8e2ee81008271ba4f5c08db3c52b94ffd
Daniel costa martins <89415617+Daniel9D@users.noreply.github.com>
2022-07-21 22:11:54 -0300

    desafio feito

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,14 +1,35 @@
 class Forca {
+  constructor(palavra_secreta){
+    this.palavra_secreta = Array.from(palavra_secreta);
 
-  chutar(letra) { }
+    this.vidas = 6;
+    this.chutadas = [];
+    this.estado = "aguardando chute";
+    this.acertadas =  Array.from(palavra_secreta);
+    this.acertadas.forEach((e,i) => this.acertadas[i]='_')
+  }
 
-  buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+  chutar(letra) {
+    if(letra.length==1){
+      let idx = this.palavra_secreta.indexOf(letra);
+      
+      if(this.chutadas.indexOf(letra)==-1){this.chutadas.push(letra); if(idx==-1) this.vidas-=1;};
+      while (idx != -1) {
+        this.acertadas[idx] = letra;
+        idx = this.palavra_secreta.indexOf(letra, idx + 1);
+      }
+    }
+  }
+
+  buscarEstado() {
+    return this.vidas <= 0 ? "perdeu" :this.acertadas.toString() == this.palavra_secreta.toString() ? "ganhou" :"aguardando chute" ; 
+  } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
 
   buscarDadosDoJogo() {
       return {
-          letrasChutadas: [], // Deve conter todas as letras chutadas
-          vidas: 6, // Quantidade de vidas restantes
-          palavra: [] // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+          letrasChutadas: this.chutadas, // Deve conter todas as letras chutadas
+          vidas: this.vidas, // Quantidade de vidas restantes
+          palavra: this.acertadas // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
       }
   }
 }
